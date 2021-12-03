af597d6338bdf271773b3c3c5151cc0dc6efaab5
daeho1909 <daeho1901@gmail.com>
2021-12-04 02:13:46 +0900

    expo updated

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+      <WebView source={{ uri: 'http://rlawogud928.cafe24.com' }} style={styles.container} />
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
