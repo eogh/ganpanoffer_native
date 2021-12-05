ac0d16d45438910d6b220e9f7f2cebc9cfbf39a9
daeho1909 <daeho1901@gmail.com>
2021-12-05 09:37:58 +0900

    - 하드웨어 백버튼 이벤트수정 - 상단겹침 문제 수정

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, BackHandler, Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
+// import { getStatusBarHeight } from "react-native-status-bar-height"; 
 
 export default function App() {
+  
+  const onAndroidBackPress = () => {
+    // BackHandler.goBack();
+    navigation.goBack();
+    return true;
+  }
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    }
+  }, []);
+
+  
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-      <WebView source={{ uri: 'http://rlawogud928.cafe24.com' }} style={styles.container} />
+      <WebView 
+        source={{ uri: 'http://rlawogud928.cafe24.com' }}
+        style={styles.container}
+      />
     </SafeAreaView>
   );
 }
@@ -15,6 +35,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    // paddingTop: getStatusBarHeight()
+    paddingTop: 20
   },
 });
